1fa89ba06f6f446820146d3563abac1ff3a1026c
wondermkhize <wondermkhize7@gmail.com>
2020-12-08 22:40:10 +0200

    API Generic Repository

---
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Linq.Expressions;
+using Core.Entities;
+using Core.Specifications;
+
+namespace eTuckshop.Core.Specifications
+{
+    public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
+    {
+        public ProductsWithTypesAndBrandsSpecification()
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
+
+        public ProductsWithTypesAndBrandsSpecification(int id) : base(x => x.Id == id)
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
+    }
+}
