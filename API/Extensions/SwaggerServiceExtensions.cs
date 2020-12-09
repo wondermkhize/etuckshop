e2c777e60fd4c7357708136eaeaf28ae65e1e590
wondermkhize <wondermkhize7@gmail.com>
2020-12-09 20:53:45 +0200

    API Error Handling

---
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace API.Extensions
+{
+    public static class SwaggerServiceExtensions
+    {
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c => 
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "eTuckshop API", Version = "v1"});
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {c
+                .SwaggerEndpoint("/swagger/v1/swagger.json", "eTuckshop API v1");});
+
+            return app;
+        }
+    }
+}
